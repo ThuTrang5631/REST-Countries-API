b5e4af0242877cea19ac20a180fa65364d463cc7
Trang Lê Thị Thu <19522382@ms.uit.edu.vn>
2023-06-15 16:54:49 +0700

    feat: create header desktop

---
+++ all-countries-app/src/Components/Header/Header.jsx
@@ -0,0 +1,12 @@
+const Header = () => {
+  return (
+    <header className="container container-header">
+      <p className="header-title">Where in the world?</p>
+      <button className="header-btn">
+        <i class="fa fa-moon-o"></i>Dark Mode
+      </button>
+    </header>
+  );
+};
+
+export default Header;
