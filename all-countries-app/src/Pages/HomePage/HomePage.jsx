ef585d7ea27da92fedcc979ec61f849ced53a3f5
Trang Lê Thị Thu <19522382@ms.uit.edu.vn>
2023-06-27 22:11:09 +0700

    feat: search and modal

--- all-countries-app/src/Pages/HomePage/HomePage.jsx
+++ all-countries-app/src/Pages/HomePage/HomePage.jsx
@@ -3,23 +3,46 @@ import CardCountry from "../../Components/CardCountry";
 import axios from "axios";
 import { getAllCountriesURL } from "../../utils/constants";
 import { useEffect, useState } from "react";
+import Modal from "../../Components/Modal";
 
 const OPTIONS = ["Africa", "America", "Asia", "Europe", "Oceania"];
 
 const HomePage = () => {
-  const [dataAllCountry, setDataAllCountry] = useState(null);
+  const [dataCountry, setDataCountry] = useState(null);
+  const [countrySearch, setCountrySearch] = useState(null);
+  const [openModal, setOpenModal] = useState(false);
 
   const getAllCountries = () => {
     axios.get(getAllCountriesURL).then((res) => {
       console.log(res);
       const data = res.data;
-      setDataAllCountry(data);
+      setDataCountry(data);
     });
   };
 
   useEffect(() => getAllCountries(), []);
 
-  console.log("data", dataAllCountry);
+  const handleChangeSearch = (e) => {
+    e.preventDefault();
+    setCountrySearch(e.target.value);
+  };
+
+  const getCountrySearch = (e) => {
+    e.preventDefault();
+    axios
+      .get(`https://restcountries.com/v3.1/name/${countrySearch}?fullText=true`)
+      .then((res) => {
+        console.log(res);
+        const data = res.data;
+        setDataCountry(data);
+      })
+      .catch((error) => {
+        console.log(error);
+        setOpenModal(true);
+      });
+  };
+
+  console.log("search", countrySearch);
   return (
     <>
       <Header />
@@ -28,11 +51,15 @@ const HomePage = () => {
           <div className="wrap-homepage-form">
             <form className="homepage-form">
               <input
+                onChange={(e) => handleChangeSearch(e)}
                 className="input"
                 type="text"
                 placeholder="Search for a country..."
               />
-              <button className="search-btn">
+              <button
+                onClick={(e) => getCountrySearch(e)}
+                className="search-btn"
+              >
                 <i className="fa fa-search"></i>
               </button>
             </form>
@@ -49,10 +76,10 @@ const HomePage = () => {
           </select>
         </div>
         <div className="list-cardcountry">
-          {dataAllCountry?.map((item) => {
+          {dataCountry?.map((item, id) => {
             return (
               <CardCountry
-                key={item?.name}
+                key={id}
                 nameCountry={item?.name?.common}
                 flagCountry={item?.flags?.png}
                 population={item?.population}
@@ -63,6 +90,11 @@ const HomePage = () => {
           })}
         </div>
       </main>
+      <Modal
+        openModal={openModal}
+        content="The country you entered is not available. Please enter another country"
+        onCancel={() => setOpenModal(false)}
+      ></Modal>
     </>
   );
 };
