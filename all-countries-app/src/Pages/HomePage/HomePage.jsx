b6b3e05b9db1d59eb75724fb168ba6e0053ec2a8
Trang Lê Thị Thu <19522382@ms.uit.edu.vn>
2023-06-30 09:43:40 +0700

    fix: bg color filter

--- all-countries-app/src/Pages/HomePage/HomePage.jsx
+++ all-countries-app/src/Pages/HomePage/HomePage.jsx
@@ -108,7 +108,7 @@ const HomePage = () => {
             className={`homepage-filter ${
               darkMode === "dark"
                 ? "dark-element-bg input-dark"
-                : "light-background"
+                : "light-text-bg"
             }`}
             style={{
               backgroundImage:
